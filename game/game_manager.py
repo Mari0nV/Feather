f7f77a9960bddea74b0c562aba85a1cb041ba346
Marion Vasseur <marion@air-de-marion.home>
2020-09-19 22:48:18 +0200

    refactor status

--- game/game_manager.py
+++ game/game_manager.py
@@ -6,23 +6,14 @@ import time
 
 class GameManager:
     def __init__(self):
-        start = time.time()
         with open('../data/dictionary.json') as json_file:
             self.dictionary = json.load(json_file)
-        print("Loading dictionary.json:", time.time() - start)
-        start = time.time()
         with open("../data/replacements.json") as json_file:
             self.replacements = json.load(json_file)
-        print("Loading replacements.json:", time.time() - start)
-        
-        start = time.time()
-        self.status = Status()
-        print("Loading Status:", time.time() - start)
-        self.dialog = Dialog(self.status)
 
-        start = time.time()
+        self.status = Status()
+        self.dialog = Dialog(self.status)
         self.sequence = ForestChapter(self)
-        print("Loading forest chapter:", time.time() - start)
     
     def intro(self):
         print("You wake up in the middle of a forest, freezing as hell." \
@@ -39,7 +30,7 @@ class GameManager:
         response = input()
         while response.lower() != "quit":
             self.sequence.process_response(response)
-            if "dead" in self.status.states["physical_state"]:
+            if "dead" in self.status["physical_state"]:
                 break
             response = input()
 
--- game/status.py
+++ game/status.py
@@ -4,22 +4,36 @@ import json
 class Status:
     def __init__(self):
         with open('../data/status.json') as json_file:
-            self.states = json.load(json_file)
+            self.status = json.load(json_file)
+    
+    def __getitem__(self, key):
+        return self.status[key]
+    
+    def __len__(self):
+        return len(self.status)
     
     def update(self, status_dict: dict):
         for status, value in status_dict.items():
             category, status = status.split('.')
-            self.states[category][status] = value
+            self.status[category][status] = value
     
     def check_status(self, status: list):
         for elt in status:
             if elt[0] == '!':
                 category, status_name, *level = elt[1:].split('.')
-                if status_name in self.states[category] and self.states[category][status_name]:
+                if status_name in self.status[category] and self.status[category][status_name]:
                     return False
             else:
                 category, status_name = elt.split('.')
-                if status_name not in self.states[category] or not self.states[category][status_name]:
+                if status_name not in self.status[category] or not self.status[category][status_name]:
                     return False
 
+        return True
+    
+    def is_alone(self):
+        if "presence" in self.status:
+            for people in self.status["presence"]:
+                if self.status["presence"][people] == True:
+                    return False
+        
         return True
