b2e24089866d08e5ee643cc787ec49153f0d4823
Marion Vasseur <marion@air-de-marion.home>
2020-09-19 23:52:56 +0200

    added dialog object

---
+++ game/dialog/dialog_dictionary.py
@@ -0,0 +1,12 @@
+import json
+
+lines = {
+    "danse": {
+        "verbs": ["danse",],
+        "next": ["around", "please", ""],
+    },
+    "hello": {
+        "verbs": ["hi", "greatings", "yo", "hey", "hello"],
+        "next": ["there", "", "you"]
+    }
+}
---
+++ game/dialog/dialog_mapping.py
@@ -0,0 +1,12 @@
+dialog_mapping = {
+    "hello": {
+        ("presence.spirit"): {
+            "msg": ["It triggers no reaction from the spirit."]
+        },
+    },
+    "danse": {
+        ("presence.spirit"): {
+            "msg": ["The spirit moves around you in a rythmic way."]
+        }
+    }
+}
