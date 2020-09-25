4e9ba57bf3d1a522320249b8e2f2f6ad73077634
Marion <marion.vasseur3@gmail.com>
2020-09-26 01:28:38 +0200

    modify env var before feather imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,15 @@
 import os
 import pytest
 
+os.environ["FEATHER_STATUS_FILE"] = "tests/fake_data/fake_status.json"
+os.environ["FEATHER_MAP_FILE"] = "tests/fake_data/fake_map.json"
+os.environ["FEATHER_SPEECH_PATH"] = "tests/fake_data/fake_speech"
+os.environ["FEATHER_DIALOG_COMBINATIONS_FILE"] = "tests/fake_data/fake_dialog_combinations.json"
+
 from feather.managers.dialog_manager import DialogManager
 from feather.managers.map_manager import MapManager
 from feather.managers.status_manager import StatusManager
 
-
-os.environ["FEATHER_STATUS_FILE"] = "tests/fake_data/fake_status.json"
-os.environ["FEATHER_MAP_FILE"] = "tests/fake_data/fake_map.json"
-
-# Dialog related paths
-os.environ["FEATHER_SPEECH_PATH"] = "tests/fake_data/fake_speech"
-os.environ["FEATHER_DIALOG_COMBINATIONS_FILE"] = "tests/fake_data/fake_dialog_combinations.json"
 
 
 class FakeOutputManager:
