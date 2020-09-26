870d33f706d9926fa42c85b001728ad86437ee05
Marion <marion.vasseur3@gmail.com>
2020-09-26 11:23:43 +0200

    added replacements file in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ os.environ["FEATHER_STATUS_FILE"] = "tests/fake_data/fake_status.json"
 os.environ["FEATHER_MAP_FILE"] = "tests/fake_data/fake_map.json"
 os.environ["FEATHER_SPEECH_PATH"] = "tests/fake_data/fake_speech"
 os.environ["FEATHER_DIALOG_COMBINATIONS_FILE"] = "tests/fake_data/fake_dialog_combinations.json"
-os.environ["FEATHER_REPLACEMENTS_FILE"] = "tests/fake_data/replacements.json"
+os.environ["FEATHER_REPLACEMENTS_FILE"] = "tests/fake_data/fake_replacements.json"
 
 from feather.managers.dialog_manager import DialogManager
 from feather.managers.map_manager import MapManager
