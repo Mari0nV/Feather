2b929af848ca5f7d6529c82eb1c2270f5012d74c
Marion <marion.vasseur3@gmail.com>
2020-09-26 20:20:13 +0200

    add history management

--- feather/managers/output_manager.py
+++ feather/managers/output_manager.py
@@ -1,3 +1,12 @@
 class OutputManager:
+    def __init__(self):
+        self.history = []
+
     def print(self, text):
+        self.history[-1]["outputs"].append(text)
         print("\n", text)
+    
+    def save_input(self, text):
+        self.history.append({
+            "input": text,
+            "outputs": []})
---
+++ tests/test_output_manager.py
@@ -0,0 +1,23 @@
+from feather.managers.output_manager import OutputManager
+
+
+def test_that_output_manager_updates_history():
+    output_manager = OutputManager()
+
+    output_manager.save_input("input1")
+    output_manager.print("output1")
+    output_manager.print("output2")
+
+    output_manager.save_input("input2")
+    output_manager.print("output3")
+
+    assert output_manager.history == [
+        {
+            "input": "input1",
+            "outputs": ["output1", "output2"]
+        },
+        {
+            "input": "input2",
+            "outputs": ["output3"]
+        },
+    ]
