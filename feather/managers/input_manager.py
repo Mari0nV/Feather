3f0f1cc4ffc3c73baa6abf558769d79f76893b24
Marion <marion.vasseur3@gmail.com>
2020-09-26 20:54:18 +0200

    input manager is now an abstract class

--- feather/managers/input_manager.py
+++ feather/managers/input_manager.py
@@ -1,70 +1,72 @@
+from abc import ABC, abstractmethod
 import json
-
 import nltk
 import random
 from autocorrect import Speller
+
 from feather.config import replacements_file
+from feather.utils.utils import remove_punctuation
 
 
-class InputManager:
-    def __init__(self):
+class InputManager(ABC):
+    def __init__(self, output_manager):
         with open(replacements_file) as json_file:
             self.replacements = json.load(json_file)
 
         self.speller = Speller()
+        self.output_manager = output_manager
 
     def _variant_responses(self, response):
         # TODO handle different verbs conjugaisons
         pass
 
-    def _preprocess_response(self, response):
+    def _clean_response(self, response):
         # check spelling
         response = self.speller(response)
+
+        # remove punctuation
+        response = remove_punctuation(response)
 
         text = nltk.word_tokenize(response.lower())
         tags = nltk.pos_tag(text)
 
-        return text, tags
-
-    def _retrieve_action(self, skinned_response):
-        pass
-
-    def _skin_response(self, text, tags):
         # replacing some words
         for i, word in enumerate(text):
             if word in self.replacements:
                 text[i] = self.replacements[word]
                 tags[i] = (self.replacements[word], tags[i][1])
 
-        skinned = ""
-        # removing superfluous
+        # removing superfluous words
+        clean_response = ""
         for word, word_type in tags:
-            if word not in ["i", "my", "you", "your"] and word_type not in [
-                "AT",
-                "DET",
-            ]:
-                skinned += f"{word} "
+            if word not in ["i", "my"] and word_type not in [
+                "AT", "DET", "DT"]:
+                clean_response += f"{word} "
 
-        return skinned[:-1]
+        return clean_response[:-1]
+
+    @abstractmethod
+    def _retrieve_action(self, clean_response):
+        pass
 
     def process_response(self, response):
-        # check spelling and decompose response
-        text, tags = self._preprocess_response(response)
+        # check spelling, replace and remove some words
+        clean_response = self._clean_response(response)
 
-        # remove subject, possessive, pronouns...
-        skinned_response = self._skin_response(text, tags)
+        # save clean response in history of inputs/outputs
+        self.output_manager.save_input(clean_response)
 
         # retrieve action from dictionary and status checking
-        action = self._retrieve_action(skinned_response)
+        action = self._retrieve_action(clean_response)
 
         # perform choosen action
         if action:
-            self._do_action(action, skinned_response)
+            self._do_action(action, clean_response)
 
-    def _do_action(self, action, skinned_resp):
+    def _do_action(self, action, clean_resp):
         if "msg" in action:
             self.output_manager.print(
-                random.choice(action["msg"]).format(action=skinned_resp)
+                random.choice(action["msg"]).format(action=clean_resp)
             )
         if "update" in action:
             self.status_manager.update(action["update"])
